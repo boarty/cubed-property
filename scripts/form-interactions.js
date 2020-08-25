45b30a6428787eba56eb929e3e527c004001e43c
boarty <32166344+boarty@users.noreply.github.com>
2020-08-25 11:58:02 +0100

    onchange events for dropdowns and feedback 1

--- scripts/form-interactions.js
+++ scripts/form-interactions.js
@@ -21,7 +21,7 @@ function showEmployedSection() {
 	noEmployed.style.display = 'none'
 	const noEmployedClass = document.getElementById('no-employed-button')
 	noEmployedClass.className = "option-buttons"
-	document.getElementById('dummy-employed').value = "Yes"
+	document.getElementById('dummy-employed').value = "Employed"
 	document.getElementById('dummy-self-employed').value = ""
 	document.getElementById('dummy-student').value = ""
 	document.getElementById('dummy-benefits').value = ""
@@ -139,7 +139,7 @@ function noEmploymentSections() {
 	document.getElementById('dummy-self-employed').value = ""
 	document.getElementById('dummy-student').value = ""
 	document.getElementById('dummy-benefits').value = ""
-	document.getElementById('dummy-no-employed').value = "No"
+	document.getElementById('dummy-no-employed').value = "Other"
 }
 
 document.getElementById('no-employed-button').onclick = noEmploymentSections
@@ -159,9 +159,9 @@ function gtEmploymentSection() {
 	document.getElementById('gt-yes-employed-button').className = 'option-buttons-selected'
 	document.getElementById('gt-self-employed-button').className = 'option-buttons'
 	document.getElementById('gt-no-employed-button').className = 'option-buttons'
-	document.getElementById('dummy-gt-yes-employed').value = "Yes"
+	document.getElementById('dummy-gt-yes-employed').value = "Employed"
 	document.getElementById('dummy-gt-self-employed').value = ""
-	document.getElementById('dummy-yes-employed').value = ""
+	document.getElementById('dummy-gt-no-employed').value = ""
 }
 
 document.getElementById('gt-yes-employed-button').onclick = gtEmploymentSection
@@ -175,7 +175,7 @@ function gtSelfEmploymentSection() {
 	document.getElementById('gt-no-employed-button').className = 'option-buttons'
 	document.getElementById('dummy-gt-yes-employed').value = ""
 	document.getElementById('dummy-gt-self-employed').value = "Self-employed"
-	document.getElementById('dummy-yes-employed').value = ""
+	document.getElementById('dummy-gt-no-employed').value = ""
 }
 
 document.getElementById('gt-self-employed-button').onclick = gtSelfEmploymentSection
@@ -189,7 +189,7 @@ function gtNoEmploymentSection() {
 	document.getElementById('gt-no-employed-button').className = 'option-buttons-selected'
 	document.getElementById('dummy-gt-yes-employed').value = ""
 	document.getElementById('dummy-gt-self-employed').value = ""
-	document.getElementById('dummy-yes-employed').value = "No"
+	document.getElementById('dummy-gt-no-employed').value = "Other"
 }
 
 document.getElementById('gt-no-employed-button').onclick = gtNoEmploymentSection
@@ -225,3 +225,59 @@ function removeProperty3() {
 }
 
 document.getElementById('remove-address3').onclick = removeProperty3
+
+
+// SHOW / HIDE PREVIOUS HOUSEMATE DIV
+
+function showPrevHousemate() {
+	document.getElementById('prev-housmemate-details').style.display = 'block'
+}
+
+document.getElementById('shared-house-yes').onclick = showPrevHousemate
+
+function hidePrevHousemate() {
+	document.getElementById('prev-housmemate-details').style.display = 'none'
+}
+
+document.getElementById('shared-house-no').onclick = hidePrevHousemate
+
+
+// SHOW / HIDE RESIDENTIAL STATUS DIV
+
+function showResidential() {
+	document.getElementById('resident-details').style.display = 'block'
+	document.getElementById('resident-passport-upload').style.display = 'none'
+}
+
+document.getElementById('british-no').onclick = showResidential
+
+function hideResidential() {
+	document.getElementById('resident-details').style.display = 'none'
+	document.getElementById('resident-passport-upload').style.display = 'block'
+}
+
+document.getElementById('british-yes').onclick = hideResidential
+
+
+// SHOW OR HIDE GUARANTOR'S HOMEOWNER DETAILS
+
+function gtHomeownerOther() {
+	var homeowner = document.getElementById('gt-homeowner-dropdown')
+	if (homeowner.value === "Other") {
+		document.getElementById('gt-homeowner-other').style.display = 'block'
+	} else {
+		document.getElementById('gt-homeowner-other').style.display = 'none'
+	}
+}
+
+
+// SHOW OR HIDE APPLICANT'S EMPLOYMENT DETAILS
+
+function employmentTypeDetails() {
+	var employmentType = document.getElementById('employment-type-dropdown')
+	if (employmentType.value === "Other") {
+		document.getElementById('employment-type-other').style.display = 'block'
+	} else {
+		document.getElementById('employment-type-other').style.display = 'none'
+	}
+}
